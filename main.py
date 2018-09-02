c8d6a1538c8cedd69fe059c7eaf9ebb1cf5cf0a1
swyh <sw_yh@naver.com>
2018-09-02 22:37:56 +0900

    analysis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from analysis import *
 
 #userID = ["rladudgns", "kyw6434", "songsungho", "sermer0267", "tjdals", "qwebnm7788", "qbration21"]
-userID = ["qwebnm7788"]
+userID = ["rladudgns"]
 list = calculate_percent(userID)
 print_rank(list)
